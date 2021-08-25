8b27b1bb74a4617d09aa4cbb3a4314221406eec5
MARIA LUIZA RIBEIRO TEIXEIRA <21193063@CFP127ALUNO.LOCAL>
2021-08-25 10:18:56 -0300

    Adicionando scripts ligar/desligar lampada a partir do movimento de mouse e quebrar lâmpada a partir de duplo clique

--- scripts/lampada.js
+++ scripts/lampada.js
@@ -5,16 +5,58 @@
 function lampOn()
 {
     const lamp = document.getElementById("lampada")
-    lamp.src = "img/ligada.jpg" 
+    const buttonDesligar = document.getElementById("desligar")
+    const buttonLigar = document.getElementById("ligar")
+
+    if (!lampIsBroken()) //se a lampIsBroken retornar false
+    {
+        lamp.src = "img/ligada.jpg" 
+        buttonDesligar.disabled = false
+        buttonLigar.disabled = true
+    }
+    
 }
 
 function lampOff()
 {
     const lamp = document.getElementById("lampada")
-    lamp.src = "img/desligada.jpg" 
+    const buttonDesligar = document.getElementById("desligar")
+    const buttonLigar = document.getElementById("ligar")
+
+    if (!lampIsBroken()) //se a lampIsBroken retornar false
+    {
+        lamp.src = "img/desligada.jpg" 
+        buttonDesligar.disabled = true
+        buttonLigar.disabled = false
+    }
+    
+}
+
+function lampBrake()
+{
+    const lamp = document.getElementById("lampada")
+    const buttonDesligar = document.getElementById("desligar")
+    const buttonLigar = document.getElementById("ligar")
+
+    lamp.src = "img/quebrada.jpg" 
+    buttonDesligar.disabled = true
+    buttonLigar.disabled = true
+}
+
+function lampIsBroken()
+{
+    const lamp = document.getElementById("lampada")
+    return lamp.src.includes("quebrada")
+    //se o src tiver a palavra quebrada, retorna true
 }
 
 // EVENTOS
 document.getElementById("ligar").addEventListener("click", lampOn)
 
 document.getElementById("desligar").addEventListener("click", lampOff)
+
+document.getElementById("lampada").addEventListener("dblclick",lampBrake)
+
+document.getElementById("lampada").addEventListener("mouseover",lampOn)
+
+document.getElementById("lampada").addEventListener("mouseleave",lampOff)
