58b51e78e6f38b05124a2941e38ec7304708ee3f
MARIA LUIZA RIBEIRO TEIXEIRA <21193063@CFP127ALUNO.LOCAL>
2021-09-01 08:34:49 -0300

    Correção professor da função piscar

---
+++ scripts/lampada-piscar-prof.js
@@ -0,0 +1,106 @@
+"use strict" //faz com que erros sejam sinalizados
+
+let idteste
+let idDesligar
+let idLigar
+
+function buttonOnOff(buttonOnState,buttonOffState )
+{
+    const buttonDesligar = document.getElementById("desligar")
+    const buttonLigar = document.getElementById("ligar")
+
+    buttonLigar.disabled = buttonOnState
+    buttonDesligar.disabled = buttonOffState
+        //recebe o estado que for passado ao invocar a função
+}
+
+function lampOn()
+{
+    const lamp = document.getElementById("lampada")
+    if (!lampIsBroken()) //se a lampIsBroken retornar false
+    {
+        lamp.src = "img/ligada.jpg" 
+        buttonOnOff(true, false) //função que liga/desliga botão
+    }
+}
+
+function lampOff()
+{
+    const lamp = document.getElementById("lampada")
+    if (!lampIsBroken()) //se a lampIsBroken retornar false
+    {
+        lamp.src = "img/desligada.jpg" 
+        buttonOnOff(false, true) //função que liga/desliga botão
+    }
+}
+
+function lampBrake()
+{
+    const lamp = document.getElementById("lampada")
+    lamp.src = "img/quebrada.jpg" 
+    buttonOnOff(true, true) //função que liga/desliga botão
+}
+
+function lampIsBroken()
+{
+    const lamp = document.getElementById("lampada")
+    return lamp.src.includes("quebrada")
+    //se o src tiver a palavra quebrada, retorna true
+}
+
+function getId(elementId)
+{
+    return document.getElementById(elementId)
+}
+
+function mensagem()
+{
+    console.log("olá mundo")
+}
+
+function testesComFuncoesDeIntervalo ()
+{
+    console.log("teste1")
+    idteste = setInterval(mensagem, 1000)
+    console.log("teste2")
+}
+
+function lampBlink()
+{
+    const buttonPiscar = document.getElementById("piscar")
+
+    if (buttonPiscar.textContent == "Piscar")
+    {
+        idDesligar = setInterval(lampOn, 500)
+        idLigar = setInterval(lampOff,1000)
+        document.getElementById("piscar").textContent = "Parar"
+    }
+
+    else
+    {
+        lamStopBlinking()
+        buttonPiscar.textContent = "Piscar"
+    }
+    
+}
+
+function lamStopBlinking () 
+{
+    clearInterval(idLigar)
+    clearInterval(idDesligar)
+}
+
+
+// EVENTOS
+getId("ligar").addEventListener("click", lampOn)
+    //usando  a função getId para conseguir o ID ao inves do document.....
+
+document.getElementById("desligar").addEventListener("click", lampOff)
+
+document.getElementById("lampada").addEventListener("dblclick",lampBrake)
+
+document.getElementById("lampada").addEventListener("mouseover",lampOn)
+
+document.getElementById("lampada").addEventListener("mouseleave",lampOff)
+
+document.getElementById("piscar").addEventListener("click", lampBlink)
